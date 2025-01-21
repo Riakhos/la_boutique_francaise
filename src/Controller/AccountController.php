965b5fc787908cb9c7356a737f333aa5b0926229
Bonnegent <richard.bonnegent@gmail.com>
2025-01-21 16:38:55 +0100

    Modify Password & Views Privates

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccountController extends AbstractController
 {
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function account(): Response
     {
-        return $this->render('account/account.html.twig', [
-            'controller_name' => 'AccountController',
+        return $this->render('account/account.html.twig');
+    }
+    
+    #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
+    public function password(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $user = $this->getUser();
+        
+        $form = $this->createForm(PasswordUserType::class, $user, [
+            'passwordHasher' => $userPasswordHasherInterface 
+        ]);
+
+        $form->handleRequest($request);
+        
+        // Si le formulaire est soumis alors :
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->flush();
+        }
+        
+        return $this->render('account/password.html.twig', [
+            'modifyPwd' => $form->createView()
         ]);
     }
 }
