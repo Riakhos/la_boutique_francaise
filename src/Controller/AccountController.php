036ee9a9dc00b45bc51c88013892a10be348b24b
Bonnegent <richard.bonnegent@gmail.com>
2025-01-29 01:22:01 +0100

    Add, Update & Delete Address with User

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressUserType;
 use App\Form\PasswordUserType;
+use App\Repository\AddressRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
+    private $entityManagerInterface;
+    
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+    {
+        $this->entityManagerInterface = $entityManagerInterface;
+    }
+    
     #[Route('/compte', name: 'app_account')]
     public function account(): Response
     {
@@ -19,7 +29,7 @@ class AccountController extends AbstractController
     }
     
     #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
-    public function password(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, EntityManagerInterface $entityManagerInterface): Response
+    public function password(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $user = $this->getUser();
         
@@ -31,7 +41,7 @@ class AccountController extends AbstractController
         
         // Si le formulaire est soumis alors :
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManagerInterface->flush();
+            $this->entityManagerInterface->flush();
             $this->addFlash(
                 'success',
                 'Votre mot de passe est correctement mis à jour.'
@@ -40,6 +50,66 @@ class AccountController extends AbstractController
         
         return $this->render('account/password.html.twig', [
             'modifyPwd' => $form->createView()
+        ]);
+    }
+
+    #[Route('/compte/adresses', name: 'app_account_addresses')]
+    public function addresses(): Response
+    {
+        return $this->render('account/addresses.html.twig');
+    }
+    
+    #[Route('/compte/adresses/delete/{id}', name: 'app_account_address_delete')]
+    public function addressDelete($id, AddressRepository $addressRepository): Response
+    {
+        $address = $addressRepository->findOneById($id);
+        
+        if (!$address OR $address->getUser()  != $this->getUser()) {
+            return $this->redirectToRoute('app_account_addresses');
+        }
+        
+        $this->addFlash(
+            'success',
+            'Votre adresse est correctement supprimée.'
+        );
+        
+        $this->entityManagerInterface->remove($address);
+        $this->entityManagerInterface->flush();
+        
+        return $this->redirectToRoute('app_account_addresses');
+    }
+    
+    #[Route('/compte/adresse/ajouter/{id}', name: 'app_account_address_form', defaults: ['id' => null] )]
+    public function addressForm(Request $request, $id, AddressRepository $addressRepository): Response
+    {
+        if ($id) {
+            $address = $addressRepository->findOneById($id);
+            if (!$address OR $address->getUser()  != $this->getUser()) {
+                return $this->redirectToRoute('app_account_addresses');
+            }
+        } else {
+            $address = new Address();
+            $address->setUser($this->getUser());
+        }
+        
+        $form = $this->createForm(AddressUserType::class, $address);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManagerInterface->persist($address);
+            $this->entityManagerInterface->flush();
+            
+            $this->addFlash(
+                'success',
+                'Votre adresse est correctement sauvegardée.'
+            );
+            
+            return $this->redirectToRoute('app_account_addresses');
+        }
+        
+        return $this->render('account/addressForm.html.twig', [
+            'addressForm'=> $form, 
         ]);
     }
 }
---
+++ src/Form/AddressUserType.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Country;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+
+class AddressUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre prénom.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 30,
+                        'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le prénom ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom'
+                ]                   
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre nom.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 30,
+                        'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le nom ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre nom'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Votre adresse',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre adresse.'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'max' => 60,
+                        'minMessage' => 'L\'adresse doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'L\'adresse ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre adresse'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Votre code postal',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre code postal.'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'max' => 8,
+                        'minMessage' => 'Le code postal doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le code postal ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Votre ville',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre ville.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 30,
+                        'minMessage' => 'La ville doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'La ville ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Votre pays',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre pays.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 30,
+                        'minMessage' => 'Le pays doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le pays ne peut pas dépasser {{ limit }} caractères.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre pays'
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Votre téléphone',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez fournir votre numéro de téléphone.'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Indiquez votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Sauvegarder',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
