2e56ae4be3683c97782f6d0aa0fa83561f9aa9cf
Bonnegent <richard.bonnegent@gmail.com>
2025-02-18 11:59:06 +0100

    Update view Easyadmin for display OrderDetails Command

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -7,9 +7,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
@@ -31,26 +31,35 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $show = Action::new('Afficher')->linkToCrudAction('show');
+        
         return $actions
+            ->add(Crud::PAGE_INDEX, $show)
             ->remove(Crud::PAGE_INDEX, Action::NEW)
-            ->remove(Crud::PAGE_DETAIL, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
             ->remove(Crud::PAGE_INDEX, Action::DELETE)
         ;
+    }
+
+    public function show(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+
+        return $this->render('admin/order.html.twig', [
+            'order' => $order
+        ]);
     }
     
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id', 'Numéro commande')
-                ->setHelp('Référence de la commande')
-            ,
-            DateField::new('createdAt', 'Statut')
-                ->setHelp('Statut de la commande')
-            ,
+            IdField::new('id', 'Numéro commande'),
+            DateField::new('createdAt', 'Date'),
+            NumberField::new('state', 'Statut')->setTemplatePath('admin/state.html.twig'),
             AssociationField::new('user', 'Utilisateur'),
-            TextField::new('carrierName', 'Transporteur')
-                ->setHelp('Transporteur de la commande')
-            ,
+            TextField::new('carrierName', 'Transporteur'),
+            NumberField::new('totalTva', 'Total TVA'),
+            NumberField::new('totalWt', 'Total TTC'),
         ];
     }
 }
