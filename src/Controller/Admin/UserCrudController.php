6588d5a35aa73f64587fba3b841d049e1f10da02
Bonnegent <richard.bonnegent@gmail.com>
2025-03-10 01:11:24 +0100

    Access Backoffice Admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -27,6 +28,14 @@ class UserCrudController extends AbstractCrudController
         return [
             TextField::new('firstname')->setLabel('Prénom'),
             TextField::new('lastname')->setLabel('Nom'),
+            ChoiceField::new('roles', 'Permissions')
+                ->setHelp('Vous pouvez choisir le rôle de cet utilisateur.')
+                ->setChoices([
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN'
+                ])
+                ->allowMultipleChoices()
+            ,
             TextField::new('email')->setLabel('Mail')->onlyOnIndex(),
         ];
     }
